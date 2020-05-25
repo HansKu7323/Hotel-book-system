b83af44fd6200ab3de2c0aa8ef9808057261e103
HansKu7323 <boy7323@gmail.com>
2020-05-25 15:35:28 +0900

    commit1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hans Hotel東京</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<h1>BOOKING & CONTACT</h1>
+<hr>
+  
+      
+       
+ 
+    
+  
+</body>
+</html>
