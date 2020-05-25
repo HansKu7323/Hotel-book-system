ebb33e9a01396fd05934467a440920a4973520b0
HansKu7323 <boy7323@gmail.com>
2020-05-25 23:27:20 +0900

    commit4

---
+++ booking_insert.php
@@ -0,0 +1,68 @@
+<?php
+//入力チェック(受信確認処理追加)
+if(
+  !isset($_POST["name"]) || $_POST["name"]=="" ||
+  !isset($_POST["email"]) || $_POST["email"]=="" ||
+  !isset($_POST["telephone"]) || $_POST["telephone"]=="" ||
+  !isset($_POST["address"]) || $_POST["address"]=="" ||
+  !isset($_POST["country"]) || $_POST["country"]=="" ||
+  !isset($_POST["arrive"]) || $_POST["arrive"]=="" ||
+  !isset($_POST["depart"]) || $_POST["depart"]=="" ||
+  !isset($_POST["people"]) || $_POST["people"]=="" ||
+  !isset($_POST["room"]) || $_POST["room"]=="" ||
+  !isset($_POST["others"]) || $_POST["others"]=="" 
+  // !isset($_POST["others"]) || $_POST["others"]==""
+){
+  exit('ParamError');
+}
+
+
+//1. POSTデータ取得
+$name = $_POST['name'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$address = $_POST['address'];
+$country = $_POST['country'];
+$arrive = $_POST['arrive'];
+$depart = $_POST['depart'];
+$people = $_POST['people'];
+$room = $_POST['room'];
+$others = $_POST['others'];
+
+
+
+//2. DB接続します(エラー処理追加)
+try {
+  $pdo = new PDO('mysql:dbname=hotel_booking_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DbConnectError:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare("INSERT INTO hotel_booking_table(id, name, email, telephone, address, country ,arrive ,depart, people, room, indate )VALUES(NULL, :name, :email, :telephone, :country, :arrive, :depart, :people, :room, :others sysdate())");
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+$stmt->bindValue(':telephone', $telephone, PDO::PARAM_STR); 
+$stmt->bindValue(':address', $address, PDO::PARAM_STR); 
+$stmt->bindValue(':country', $country, PDO::PARAM_STR); 
+$stmt->bindValue(':arrive', $arrive, PDO::PARAM_STR); 
+$stmt->bindValue(':depart', $depart, PDO::PARAM_STR); 
+$stmt->bindValue(':people', $people, PDO::PARAM_INT); 
+$stmt->bindValue(':room', $room, PDO::PARAM_STR); 
+$stmt->bindValue(':others', $others, PDO::PARAM_STR); 
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("QueryError:".$error[2]);
+
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: index.php");
+  exit;
+
+}
+?>
--- index.php
+++ index.php
@@ -3,65 +3,69 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hans Hotel東京</title>
+  <title>Hans Hotel Japan</title>
   <link rel="stylesheet" href="css/reset.css">
   <link href="css/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
-  <!-- <link href="css/bootstrap.min.css" rel="stylesheet">
-</head> -->
+  <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+</head>
 
 <body>
+  <header>
+    <!-- <h1>Hans Hotel Japan</h1> -->
+  </header>
+  <div class="container">
+      <div class="mainbg">Hans Hotel Japan</div>
+  </div>
 
-<div class="container">
-  
-  <form action="">
-    <section class="guest data"> 
-      <h2>BOOKING & CONTACT</h2>
-      <hr>
-      <ul>
-      <li><label for="name">Name</label>
-      <input type="text" name="" id="name"></li>
-      <li>  <label for="email">Email</label>
-      <input type="email" name="" id="email"></li>
-      <li> <label for="telephone">Telephone</label>
-      <input type="tel" name="" id="telephone"></li>
-      <li>  <label for="address">Address</label>
-      <input type="text" name="" id="address"></li>
-      <li>  <label for="country">Country</label>
-      <input type="text" name="" id="country"></li>
-      </ul>
-    </section>
 
-    <section class="details">
-      <h2>DETAILS</h2>
-      <hr>
-      <ul>
-      <li> <label for="arrive">Arrive</label>
-      <input type="date" name="" id="arrive">
-      <label for="depart">Depart</label>
-      <input type="date" name="" id="depart"></li>
-      <li><label for="people">People</label>
-      <input type="number" name="" id="people">
-      <label for="room">Room</label>
-      <select name="Room" id="room" >
-        <option value="sng">SNG room</option>
-        <option value="twn">TWN room</option>
-        <option value="dbl">DBL room</option>
-        <option value="trp">TRP room</option>
-        <option value="quad">QUAD room</option>
-      </select></li>
-      <li> <label for="others">Others</label>
-      <textarea name="" id="others" cols="30" rows="10"></textarea></li>
-      <li><input type="submit" value="submit"></li>
-      </ul>
-    </section>
-  </form>
-</div>
+  <section class="form">
+    <form method="post" action="booking.php" >
+      <section class="guest data"> 
+        <h2>BOOKING & CONTACT</h2>
+        <hr>
+        <ul>
+        <li><label for="name">Name</label>
+        <input type="text" name="name" id="name"></li>
+        <li>  <label for="email">Email</label>
+        <input type="email" name="email" id="email"></li>
+        <li> <label for="telephone">Telephone</label>
+        <input type="text" name="telephone" id="telephone"></li>
+        <li>  <label for="address">Address</label>
+        <input type="text" name="address" id="address"></li>
+        <li>  <label for="country">Country</label>
+        <input type="text" name="country" id="country"></li>
+        </ul>
+      </section>
 
-  
-      
-       
- 
-    
-  
+      <section class="details">
+        <h2>DETAILS</h2>
+        <hr>
+        <ul>
+        <li> <label for="arrive">Arrive</label>
+        <input type="date" name="arrive" id="arrive">
+        <label for="depart">Depart</label>
+        <input type="date" name="depart" id="depart"></li>
+        <li><label for="people">People</label>
+        <input type="number" name="people" id="people">
+        <label for="room">Room</label>
+        <select name="Room" id="room" >
+          <option value="sng">SNG room</option>
+          <option value="twn">TWN room</option>
+          <option value="dbl">DBL room</option>
+          <option value="trp">TRP room</option>
+          <option value="quad">QUAD room</option>
+        </select></li>
+        <li> <label for="others">Others</label>
+        <textarea name="" id="others" cols="30" rows="10"></textarea></li>
+
+        <button type="submit" class="btn">Submit</button>
+        </ul>
+      </section>
+    </form>
+  </section>
+
+  <footer>
+    <p>(c) Hans Hotel.com</p>
+  </footer>
 </body>
 </html>
