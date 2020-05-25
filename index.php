21ccf8c18944d9f2218670f3fb873e0a6d4fcf59
HansKu7323 <boy7323@gmail.com>
2020-05-25 18:13:08 +0900

    commit2

--- index.php
+++ index.php
@@ -10,8 +10,39 @@
 
 <body>
 
-<h1>BOOKING & CONTACT</h1>
+<h2>BOOKING & CONTACT</h2>
 <hr>
+<form action="">
+<label for="name">Name</label>
+<input type="text" name="" id="name">
+<label for="email">Email</label>
+<input type="email" name="" id="email">
+<label for="telephone">Telephone</label>
+<input type="text" name="" id="telephone">
+<label for="address">Address</label>
+<input type="text" name="" id="address">
+<label for="country">Country</label>
+<input type="text" name="" id="country">
+
+<h2>DETAILS</h2>
+<hr>
+<label for="arrive">Arrive</label>
+<input type="date" name="" id="arrive">
+<label for="depart">Depart</label>
+<input type="date" name="" id="depart">
+<label for="people">People</label>
+<input type="number" name="" id="people">
+<label for="room">Room</label>
+<select name="Room" id="room" >
+  <option value="sng">SNG room</option>
+  <option value="twn">TWN room</option>
+  <option value="dbl">DBL room</option>
+  <option value="trp">TRP room</option>
+  <option value="quad">QUAD room</option>
+</select>
+
+
+</form>
   
       
        
